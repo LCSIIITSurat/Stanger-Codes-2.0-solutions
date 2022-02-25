c2359898cbdc6580ffc385df55cae1272c02326e
LCSIIITSurat <lcs@iiitsurat.ac.in>
2022-02-25 10:49:22 +0530

    Create Q1_Strange_Grids.cpp

---
+++ Q1_Strange_Grids.cpp
@@ -0,0 +1,23 @@
+    #include <bits/stdc++.h>
+     
+    using namespace std;
+     
+    int main() {
+    	int num;
+    	cin >> num;										// Reading input from STDIN
+    	// Writing output to STDOUT
+     
+    	while(num--){
+    		int a,b,c,d,e,f;
+    		cin>>a>>b>>c>>d>>e>>f;
+     
+    		int l1=abs(e-a)+abs(f-b);
+    		int l2 = max(abs(e-c),abs(f-d));
+            
+            if(l1<l2)
+        		cout<<"1\n";
+            else
+        		cout<<"0\n";
+               
+    	}
+    }
